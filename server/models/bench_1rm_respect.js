6a585dd1da0d8e504517e3f3a3339e1211ede1e5
jiwoonghwang <hyae4924@gmail.com>
2022-06-02 03:40:23 +0900

    [FEAT] Add Bench_1rm_respect  model & migration association

--- server/models/bench_1rm_respect.js
+++ server/models/bench_1rm_respect.js
@@ -4,21 +4,32 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Bench_1rm_respect extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+   
     static associate(models) {
-      // define association here
+       //M:1 Bench_1rm_like:User
+       this.belongsTo(models.User, {
+        foreignKey: "user_id",
+        onDelete: "cascade",
+      });
+      //M:1 Bench_1rm_like:Bench_1rm
+      this.belongsTo(models.Bench_1rm, {
+        foreignKey: "bench_1rm_id",
+        onDelete: "cascade",
+      });
     }
   }
   Bench_1rm_respect.init({
-    user_id: DataTypes.INTEGER,
-    bench_1rm_id: DataTypes.INTEGER
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    bench_1rm_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Bench_1rm_respect',
   });
   return Bench_1rm_respect;
-};
+}; 
--- server/models/ohwunwan_like.js
+++ server/models/ohwunwan_like.js
@@ -6,12 +6,12 @@ module.exports = (sequelize, DataTypes) => {
   class Ohwunwan_like extends Model {
 
     static associate(models) {
-      //M:1 Owunwan_like:User
+      //M:1 Ohwunwan_like:User
       this.belongsTo(models.User, {
         foreignKey: "user_id",
         onDelete: "cascade",
       });
-      //M:1 Owunwan_like:Ohwunwan
+      //M:1 Ohwunwan_like:Ohwunwan
       this.belongsTo(models.Ohwunwan, {
         foreignKey: "ohwunwan_id",
         onDelete: "cascade",
