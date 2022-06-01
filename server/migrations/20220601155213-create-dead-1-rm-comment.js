cd1529bdd079c1ba5a5e585ed322ea3c63b89202
jiwoonghwang <hyae4924@gmail.com>
2022-06-02 04:15:54 +0900

    [FEAT] Add Dead_1rm_comment model & migration association

--- server/migrations/20220601155213-create-dead-1-rm-comment.js
+++ server/migrations/20220601155213-create-dead-1-rm-comment.js
@@ -9,19 +9,32 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: "cascade",
+        references: {
+          model: "Users",
+          key: "id",
+        },
       },
       dead_1rm_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: "cascade",
+        references: {
+          model: "Dead_1rms",
+          key: "id",
+        },
       },
       text_content: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
--- server/models/dead_1rm_comment.js
+++ server/models/dead_1rm_comment.js
@@ -4,19 +4,33 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Dead_1rm_comment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
-    }
+      //M:1 Dead_1rm_comment:User
+      this.belongsTo(models.User, {
+        foreignKey: "user_id",
+        onDelete: "cascade",
+      });
+      //M:1 Dead_1rm_comment:Dead_1rm
+      this.belongsTo(models.Dead_1rm, {
+        foreignKey: "dead_1rm_id",
+        onDelete: "cascade",
+      });
+    } 
   }
   Dead_1rm_comment.init({
-    user_id: DataTypes.INTEGER,
-    dead_1rm_id: DataTypes.INTEGER,
-    text_content: DataTypes.STRING
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    dead_1rm_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    text_content:{ 
+      type:DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Dead_1rm_comment',
