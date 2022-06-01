4f30084e0ecebb9f0119152dfbd1aecb139908ed
jiwoonghwang <hyae4924@gmail.com>
2022-06-02 04:28:37 +0900

    [FEAT] Add Squat_1rm_comment model & migration association

--- server/models/squat_1rm_comment.js
+++ server/models/squat_1rm_comment.js
@@ -4,19 +4,33 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Squat_1rm_comment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
+      //M:1 Squat_1rm_comment:User
+      this.belongsTo(models.User, {
+        foreignKey: "user_id",
+        onDelete: "cascade",
+      });
+      //M:1 Squat_1rm_comment:Squat_1rm
+      this.belongsTo(models.Squat_1rm, {
+        foreignKey: "squat_1rm_id",
+        onDelete: "cascade",
+      });
     }
   }
-  Squat_1rm_comment.init({
-    user_id: DataTypes.INTEGER,
-    squat_1rm_id: DataTypes.INTEGER,
-    text_content: DataTypes.STRING
+  Squat_1rm_comment.init({ 
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    squat_1rm_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    text_content:{ 
+      type:DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Squat_1rm_comment',
