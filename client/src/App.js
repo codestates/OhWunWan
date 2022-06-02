a0c6b236bffc882b6468de04d6f6dc8bd5877cae
hackjoong <nuclearjoong@gmail.com>
2022-06-02 11:56:54 +0900

    [FEAT] ADD Kakao OAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// Page
+import Welcome from "./OAuth/Welcome";
+import Loding from "./OAuth/Loding";
+
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />}></Route>
+        <Route path="/oauth/callback/kakao" element={<Loding />}></Route>
+      </Routes>
+  </Router>
   );
 }
 
