a39fc493f7e1c243905908c7dfc4595caa9e7130
hackjoong <nuclearjoong@gmail.com>
2022-06-06 16:53:15 +0900

    [FEAT] Add Styled Component

---
+++ client/src/Styled.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    border-color: #EBEBEB;
+    display: flex;
+    align-self: center;
+    justify-content: center;
+  }
+` 
+
+export default GlobalStyle
