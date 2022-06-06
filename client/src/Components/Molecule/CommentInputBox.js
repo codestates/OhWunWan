9539755dd74f17643d117c7eb12082790675946f
hackjoong <nuclearjoong@gmail.com>
2022-06-06 16:45:27 +0900

    [FEAT] Update CommentInputBox Component

--- client/src/Components/Molecule/CommentInputBox.js
+++ client/src/Components/Molecule/CommentInputBox.js
@@ -1,24 +1,22 @@
-import React, { Fragment } from 'react';
-import styled from 'styled-components';
-import Input from '../Atoms/Input';
-import Submit from '../Atoms/Submit';
+import styled from "styled-components"
+import STYLE from "../../config"
+import PostInput from "../Atoms/PostInput"
+import SubmitButton from "../Atoms/SubmitButton"
 
-const StyledCommentInputBox = styled.form`
-  display:flex;
-  justify-content: space-between;
-  border: 1px solid black;
-  width: 30vw;
-  `
+const Div = styled.div`
+  width: ${STYLE.WIDTH};
+  border: 0.1em solid ${STYLE.BORDER_COLOR};
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`
 
-function CommentInputBox(){
+function CommentInputBox() {
   return (
-    <Fragment>
-    <StyledCommentInputBox action='데이터를 보낼 서버 페이지의 주소'>
-      <Input props={"을 입력해주세요"}>
-      </Input>
-      <Submit></Submit>
-    </StyledCommentInputBox>
-    </Fragment>
+    <Div>
+      <PostInput comment="comment" />
+      <SubmitButton />
+    </Div>
   )
 }
 
