6226d4fa98585b26fd0a57d4ccf406a79e1d7205
hackjoong <nuclearjoong@gmail.com>
2022-06-06 16:50:49 +0900

    [FEAT] Update HeaderBlock Component

--- client/src/Components/Organism/HeaderBlock.js
+++ client/src/Components/Organism/HeaderBlock.js
@@ -1,25 +1,48 @@
-import React, { Fragment } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-import styled from 'styled-components';
-import HeaderButton from '../Atoms/HeaderButton';
+import styled from "styled-components"
+import { Fragment } from "react";
 
-const StyledDiv=styled.div`
-    display:flex;
+// img
+import logo from "../Picture/Logo/logo.svg"
+import post from "../Picture/HeaderButton/post.png";
+import ohwunwan from "../Picture/HeaderButton/ohwunwan.png";
+import onerm from "../Picture/HeaderButton/1rm.png";
+import feedback from "../Picture/HeaderButton/feedback.png";
+import user from "../Picture/HeaderButton/user.png";
+
+// Atmos
+import HeaderButton from "../Atoms/HeaderButton";
+import Logo from "../Atoms/Logo";
+
+const Div = styled.div`
+  width: 100vw;
+  height: 3.2em;
+  border: 0.1em solid #000;
+  text-align: center;
+  vertical-align: middle;
 `
 
-function HeaderBlock(){
+const Div2 = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0.1em;
+  right: 0;
+`
 
-    return(
+function HeaderBlock() {
+  return(
     <Fragment>
-        <StyledDiv>
-        <HeaderButton></HeaderButton>
-        <HeaderButton></HeaderButton>
-        <HeaderButton></HeaderButton>
-        <HeaderButton></HeaderButton>
-        <HeaderButton></HeaderButton>
-        </StyledDiv>
+    <Div>
+      <Logo />
+      <Div2>
+        <HeaderButton img={post} circle='circle' />
+        <HeaderButton img={ohwunwan} />
+        <HeaderButton img={onerm} />
+        <HeaderButton img={feedback} />
+        <HeaderButton img={user} circle='circle' />
+      </Div2>
+    </Div>
     </Fragment>
-    )
+  )
 }
 
 export default HeaderBlock
