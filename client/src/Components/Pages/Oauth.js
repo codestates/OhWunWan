b70da7b8e8657262f6bbf9f6f044ce662f951554
hackjoong <nuclearjoong@gmail.com>
2022-06-07 03:28:18 +0900

    [FEAT] Update Oauth Page

--- client/src/Components/Pages/Oauth.js
+++ client/src/Components/Pages/Oauth.js
@@ -12,11 +12,17 @@ const Div = styled.div`
 `
 
 function Oauth() {
+  function redirect() {
+    window.location.replace("https://localhost:3000/ohwunwan");
+  }
+
   useEffect(() => {
     const url = new URL(window.location.href);
     const code = url.searchParams.get('code')
     // console.log(code)
-    axios.post('https://localhost:4000/auth/kakao',{code}).then((result)=>console.log(result))
+    axios.post('https://localhost:4000/auth/kakao',{code})
+      .then((result)=>console.log(result))
+      .then(redirect)
   }, [])
 
   return(
