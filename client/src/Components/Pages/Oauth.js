99741693da647ee53cd026cc2ab98889c617c91d
hackjoong <nuclearjoong@gmail.com>
2022-06-07 02:46:35 +0900

    [FEAT] Update Oauth Page

--- client/src/Components/Pages/Oauth.js
+++ client/src/Components/Pages/Oauth.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Loding from "../Atoms/Loding";
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const Div = styled.div`
   width: 100vw;
@@ -10,6 +12,13 @@ const Div = styled.div`
 `
 
 function Oauth() {
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const code = url.searchParams.get('code')
+    // console.log(code)
+    axios.post('https://localhost:4000/auth/kakao',{code}).then((result)=>console.log(result))
+  }, [])
+
   return(
     <Div>
       <Loding />
