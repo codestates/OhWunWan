92e8f9193e8d2c13d59807e2245fabeac8f312d6
jiwoonghwang <hyae4924@gmail.com>
2022-06-08 01:09:37 +0900

    [Fix] FIx function about logOut

--- server/controllers/auth/logout.js
+++ server/controllers/auth/logout.js
@@ -2,7 +2,8 @@ module.exports={
     //로그아웃
     post:async (res,req)=>{
         try{
-            return res.clearCookie(access_token).send({ message: 'Successfully logged out' });
+            if(!req.cookies.access_token) return res.json({ message: 'Bad Request!' })
+            return res.clearCookie(access_token).json({ message: 'Successfully logged out' });
         }catch(err){
             console.log(err);
             res.status(500).json({ message: 'Server Error!' })
