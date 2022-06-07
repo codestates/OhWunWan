68dfb498e14d6dd276144ac9031a3002b24bcb84
jiwoonghwang <hyae4924@gmail.com>
2022-06-07 23:14:29 +0900

    [FIX] Fix messages of auth/kakao.js

--- server/controllers/auth/kakao.js
+++ server/controllers/auth/kakao.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const axios = require('axios');
 const { User } = require('../../models');
+const { get_user_info,
+    send_access_token
+} = require("../tokenFunctions")
 
 module.exports = {
     post: async (req, res) => {
@@ -14,7 +17,7 @@ module.exports = {
             if (!code) return res.status(400).json({ message: 'Bad Request!' })
 
 
-            //1.카카오 토큰 요청과 응답
+            //1.카카오 로그인 요청과 응답(토큰제공)
             const response = await axios({
                 method: 'post',
                 url: `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.REST_API_KEY}&redirect_uri=${process.env.REDIRECT_URI}&code=${code}`,
@@ -24,20 +27,14 @@ module.exports = {
             })
 
 
-            //카카오 access_token,refresh_token
-            const { access_token, refresh_token } = response.data
-            // console.log('!!!!!!!!!', access_token,refresh_token)
+            //카카오 access_token
+            const { access_token } = response.data
+            // console.log('!!!!!!!!!', access_token)
 
 
-            //2.받아온 토큰으로 카카오에게 유저의 정보 요청하기
-            const user_info = await axios({
-                method: 'post',
-                url: `https://kapi.kakao.com/v2/user/me`,
-                headers: {
-                    Authorization: `Bearer ${access_token}`,
-                    'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-                },
-            })
+            //2.access_tkoen으로 카카오에게 유저의 정보 요청하기
+            const user_info = get_user_info(access_token)
+            //console.log("%%%%%%%%%%%%%",user_info)
 
 
             //카카오에서 받아온 유저의 고유번호 & //유저의 닉네임 및 사진
@@ -51,24 +48,26 @@ module.exports = {
             // console.log('!!!!!!!!!!!!!!!!',check_user_info)
 
 
-            //신 유저의 경우 db에 생성
+            //3.1 신 유저의 경우 
             if (!check_user_info) {
                 const new_user = {
                     kakao_id,
                     nickname,
                     profile_picture,
                 }
+
+
+                //신규유저 db에 생성
                 const user = await User.create(new_user)
                 // console.log('@@@@@@@@@@@',user.data)
 
-                //토큰보내기, 유저 정보 보내기
-                res.cookie("refresh_token", refresh_token, {
-                    httpOnly: true, secure: true
-                });
-                res.cookie("access_token", access_token, {
-                    httpOnly: true, secure: true
-                });
-                return res.json({
+
+                //토큰보내기 
+                send_access_token(req, access_token);
+
+
+                //유저 정보 보내기
+                return res.status(201).json({
                     message: 'This is new user info', data: {
                         user_info: user.dataValues
                     }
@@ -76,22 +75,25 @@ module.exports = {
             }
 
 
-            //기존유저인 경우
-            res.cookie("refresh_token", refresh_token, {
-                httpOnly: true, secure: true
-            });
-            res.cookie("access_token", access_token, {
-                httpOnly: true, secure: true
-            });
-            return res.status(201).json({
+            //3.2 기존유저인 경우
+            //토큰전송
+            send_access_token(req, access_token)
+            //유저정보보내기
+            return res.status(200).json({
                 message: 'This is user info', data: {
                     user_info: check_user_info.dataValues
                 }
-                
             })
+
+            //에러잡기
         } catch (err) {
             console.log(err)
             res.status(500).json({ message: 'Server Error!' })
         }
     }
 }
+
+
+
+
+
