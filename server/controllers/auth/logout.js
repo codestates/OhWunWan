afb63840705bed7f2bf34ed1be2e31d48e2528b4
jiwoonghwang <hyae4924@gmail.com>
2022-06-08 01:05:46 +0900

    [FEAT] Add function about logOut

--- server/controllers/auth/logout.js
+++ server/controllers/auth/logout.js
@@ -1,3 +1,11 @@
 module.exports={
-    post:()=>{} //로그아웃
+    //로그아웃
+    post:async (res,req)=>{
+        try{
+            return res.clearCookie(access_token).send({ message: 'Successfully logged out' });
+        }catch(err){
+            console.log(err);
+            res.status(500).json({ message: 'Server Error!' })
+        }
+    } 
 }
