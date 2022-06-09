a86f25ec244ef683a57bf54ec06f71ed4dc47930
hackjoong <nuclearjoong@gmail.com>
2022-06-09 14:17:09 +0900

    [FEAT] Update SubmitButton Component

--- client/src/Components/Atoms/SubmitButton.js
+++ client/src/Components/Atoms/SubmitButton.js
@@ -5,10 +5,14 @@ const Button = styled.span`
   border-radius: 0.3em;
   user-select: none; // 드래그 금지
   display:inline-block;
-  color: ${props => props.type === 'comment' ? '#2D9BF0' : '#fff'};
+  color: ${props => props.type === 'comment' ? '#2D9BF0' : 
+  props.type === 'red' ? '#F24726' :
+  props.type === 'black' ? '#000' : '#fff'};
   padding: ${props => props.type === 'comment' ? 'none' : '0.1em;'};
   background-color: ${
     props => props.type === 'comment' ? 'none' : 
+    props.type === 'black' ? 'none' : 
+    props.type === 'red' ? 'none' : 
     props.type === 'content' ? '#2D9BF0' :
     props.type === 'nickname' ? '#2D9BF0' : '#F24726'
   };
@@ -21,9 +25,9 @@ SubmitButton.defaultProps = {
   onClick: () => {console.log('제출 버튼 동작')}
 }
 
-function SubmitButton({text, type, onClick}) {
+function SubmitButton({text, type, onClick, color}) {
   return(
-    <Button onClick={onClick} type={type} >{text}</Button>
+    <Button onClick={onClick} type={type} color={color} >{text}</Button>
   )
 }
 
