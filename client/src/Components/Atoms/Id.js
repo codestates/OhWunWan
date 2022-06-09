e262181fbdd323d7f44bbfc6f46baa5aecaaecd1
hackjoong <nuclearjoong@gmail.com>
2022-06-09 14:13:52 +0900

    [FEAT] Update Id Component

--- client/src/Components/Atoms/Id.js
+++ client/src/Components/Atoms/Id.js
@@ -1,18 +1,22 @@
 import styled from "styled-components"
 
 const Name = styled.span`
-  font-size: 1em;
+  font-size: ${props => props.type === 'user' ? '1.6em' : '1em'};
   margin-left: 0.2em;
+  width: 5em;
   user-select: none; // 드래그 금지
+  /* border: 0.1em solid #000; */
+  font-weight: ${props => props.type === 'user' ? 'bold' : 
+  props.type === 'mypage' ? 'bold' : 'none'}
 `
 
 Id.defaultProps = {
   nickname: 'codestates'
 }
 
-function Id({nickname}) {
+function Id({nickname, type}) {
   return(
-    <Name>{nickname}</Name>
+    <Name type={type}>{nickname}</Name>
   )
 }
 
