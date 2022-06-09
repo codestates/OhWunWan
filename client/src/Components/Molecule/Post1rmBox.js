8dadc76270f13ed8ac25723ece9d792d74d6a9d3
hackjoong <nuclearjoong@gmail.com>
2022-06-09 13:49:24 +0900

    [FEAT] Add Post1rmBox Component

---
+++ client/src/Components/Molecule/Post1rmBox.js
@@ -0,0 +1,47 @@
+import styled from "styled-components"
+import STYLE from "../../config"
+import PostSubject from "../Atoms/PostSubject"
+import PostInput2 from "../Atoms/PostInput2"
+import PostSelect from "../Atoms/PostSelect"
+
+const Div = styled.div`
+  /* width: ${STYLE.WIDTH};
+  height: 3em;
+  border: 0.1em solid ${STYLE.BORDER_COLOR}; */
+`
+
+const Div2 = styled.div`
+  width: ${STYLE.WIDTH};
+  border: 0.1em solid ${STYLE.BORDER_COLOR};
+  padding: 0.2em 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Div3 = styled.div`
+  width: 20%;
+  display: flex;
+  align-items: center;
+  /* border: 0.1em solid #000; */
+`
+
+function Post1rmBox() {
+  return (
+    <Div>
+      <Div2>
+        <PostSubject text='종목선택' />
+        <PostSelect />
+      </Div2>
+      <Div2>
+        <PostSubject text='1RM' />
+        <Div3>
+          <PostInput2 />
+          <span>KG</span>
+        </Div3>
+      </Div2>
+    </Div>
+  )
+}
+
+export default Post1rmBox
