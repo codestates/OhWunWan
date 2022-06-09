6769985066596be65eff220febd843ba01f1fa71
hackjoong <nuclearjoong@gmail.com>
2022-06-09 13:40:21 +0900

    [FEAT] Add LoginButton Component

---
+++ client/src/Components/Atoms/LoginButton.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+const Button = styled.span`
+  /* border: 0.1em solid #000; */
+  font-size: 1.5em;
+  padding: 0.2em;
+  border-radius: 0.4em;
+  :hover{
+    color: blue;
+  }
+`
+
+LoginButton.defaultProps = {
+  text: '오운완 서비스를 시작하시겠습니까?',
+}
+
+function LoginButton({text, onClick}) {
+  return(
+    <Link to='/oauth/kakao'>
+      <Button>{text}</Button>
+    </Link>
+  )
+}
+
+export default LoginButton
