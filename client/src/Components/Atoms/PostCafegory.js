1544bc6610a010eb28deb8e591faf55b7d12ed93
hackjoong <nuclearjoong@gmail.com>
2022-06-09 13:41:51 +0900

    [FEAT] Update PostCafegory Component

--- client/src/Components/Atoms/PostCafegory.js
+++ client/src/Components/Atoms/PostCafegory.js
@@ -3,12 +3,15 @@ import styled from "styled-components"
 const Button = styled.span`
   user-select: none; // 드래그 금지
   cursor: pointer;
+  width: 6em;
+  color: ${props => props.select === 'select' ? 'blue' : '#000'};
+  text-align: center;
 `
 
-function PostCafgory({subject}) {
+function PostCategory({subject, select, onClick}) {
   return (
-    <Button>{subject}</Button>
+    <Button select={select} onClick={onClick}>{subject}</Button>
   )
 }
 
-export default PostCafgory
+export default PostCategory
