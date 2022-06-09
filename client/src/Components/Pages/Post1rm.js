882570ce7610c8b9862dab99b306ddc50686f83b
hackjoong <nuclearjoong@gmail.com>
2022-06-09 14:06:15 +0900

    [FEAT] Add Post1rm Component

---
+++ client/src/Components/Pages/Post1rm.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+import STYLE from "../../config"
+import PostingBlock2 from "../Organism/PostingBlock2"
+import HeaderBlock from "../Organism/HeaderBlock"
+import MarginBox from "../Atoms/MariginBox"
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+`
+
+function Post1rm() {
+  return(
+    <Div>
+      <MarginBox />
+      <HeaderBlock />
+      <PostingBlock2 />
+    </Div>
+  )
+}
+
+export default Post1rm
