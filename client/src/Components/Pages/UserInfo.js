39c17d667f8afe20d9fafc9ee7c7722a38b7799a
hackjoong <nuclearjoong@gmail.com>
2022-06-09 14:09:50 +0900

    [FEAT] Update UserInfo Component

---
+++ client/src/Components/Pages/UserInfo.js
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+import HeaderBlock from "../Organism/HeaderBlock"
+import MarginBox from "../Atoms/MariginBox"
+import UserInfoBlock from "../Organism/UserInfoBlock"
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+`
+
+function UserInfo() {
+  return(
+    <Div>
+      <HeaderBlock />
+      <MarginBox />
+      <UserInfoBlock />
+    </Div>
+  )
+}
+
+export default UserInfo
