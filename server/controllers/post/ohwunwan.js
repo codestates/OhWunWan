5f3c57264de64264aee5e7d9ff651f94eebaabcd
jiwoonghwang <hyae4924@gmail.com>
2022-06-11 01:22:40 +0900

    [FEAT] Add  ohwunwan post api

--- server/controllers/post/ohwunwan.js
+++ server/controllers/post/ohwunwan.js
@@ -4,21 +4,27 @@ const { Ohwunwan } = require('../../models');
 module.exports = {
     //ohwunwan게시물 조회
     get: async (req, res) => {
-        
+
     },
     //ohwunwan게시물 생성
     post: async (req, res) => {
-        const { user_id, text_content } = req.body
-        console.log('::::::::::::::::user_id:', user_id)
-        console.log(req.file)
-        const { path } = req.file
-        const post_info = {
-            user_id,
-            text_content,
-            picture: path
+        try {
+            const { user_id, text_content } = req.body
+            // console.log('::::::::::::::::user_id:', user_id)
+            const { location } = req.file
+            //console.log(':::::::::::::location:',location)
+            if (!user_id && !text_content && !location) return res.status(400).json({ message: 'Bad Request!' })
+            const post_info = {
+                user_id,
+                text_content,
+                picture: location
+            }
+            await Ohwunwan.create(post_info);
+
+            res.status(201).json({ message: 'The post has been created' })
+        } catch (err) {
+            res.status(500).json({ message: 'Server Error!' })
         }
-        Ohwunwan.create(post_info);
-        res.json({ message: 'ok' })
     },//ohwunwan게시묵 작성
     patch: () => { },//ohwunwan게시물 수정
     delete: () => { },//ohwunwan게시물 삭제
