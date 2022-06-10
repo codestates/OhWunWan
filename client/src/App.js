6555cdfe1473b7b27a5084991110ce1a4eb7c9e7
seungjun0423 <sibonum@gmail.com>
2022-06-11 00:05:40 +0900

    [FEAT] Add sustaining user_info

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,useNavigate} from "react-router-dom";
+import { useState, useEffect } from 'react';
+import {useSelector, useDispatch} from "react-redux" 
+import axios from 'axios';
+import { IsLogin, IsLogout, GetUserInfo } from "./Ducks/Slice/AuthSlice.js"
+
 
 // GlobalStyle
 import GlobalStyle from "./Styled";
@@ -17,21 +22,65 @@ import Mypage from "./Components/Pages/Mypage";
 import UserInfo from "./Components/Pages/UserInfo";
 import Menu from "./Components/Pages/Menu"
 
-import OhWunWan2 from "./Components/Pages/OhWunWan2";
-import OhWunWan3 from "./Components/Pages/OhWunWan3";
-import PostingBlock from './Components/Organism/PostingBlock';
-
 function App() {
+
+  const is_login = useSelector((state)=>state.auth.login)
+  const user_info = useSelector((state)=>state.auth.user_info)
+  const dispatch = useDispatch()
+  const url = new URL(window.location.href);
+  const code = url.searchParams.get('code')
+
+  const authentication = async () => {
+    try {
+      const res = await axios.get('https://localhost:4000/auth',{withCredentials:true})
+      console.log("get요청에 대한 서버응답",res.data.data.user_info)
+
+      if (res.data.data.user_info) {
+        dispatch(IsLogin());
+        dispatch(GetUserInfo(res.data.data.user_info));
+      } else {
+        dispatch(IsLogout());
+      }
+    } catch (err) {
+      dispatch(IsLogout());
+    }
+  };
+
+  const redirect= ()=> {
+    axios.post('https://localhost:4000/auth/kakao',{code})
+    .then((res)=>{console.log("post요청에 대한 서버응답",res.data.data.user_info);return res.data.data.user_info})
+    .then((res)=>{window.location.replace("https://localhost:3000/ohwunwan")})
+  }
+
+
+  //1번실행
+  useEffect(() => {
+    authentication();
+    if(code)redirect()
+  }, []);
+  
+  console.log("로그인",is_login)
+  console.log("리덕스에 저장된 유저정보",user_info)
+  
   return (
     <Router>
       <GlobalStyle />
-
       <Routes>
-        <Route path="/" element={<PostingBlock></PostingBlock>}></Route>
-        
+        <Route path="/" element={<Welcome />}></Route>
+        <Route path="/oauth/kakao" element={<Login />}></Route>
+        <Route path="/ohwunwan" element={<OhWunWan />}></Route>
+        <Route path="/1rm" element={<Onerm />}></Route>
+        <Route path="/feedback" element={<Feedback />}></Route>
+        <Route path="/post" element={<PostCategory />}></Route>
+        <Route path="/post/ohwunwan" element={<PostOhwunwan />}></Route>
+        <Route path="/post/1rm" element={<Post1rm />}></Route>
+        <Route path="/post/feedback" element={<PostFeedback />}></Route>
+        <Route path="/user" element={<Mypage />}></Route>
+        <Route path="/userinfo" element={<UserInfo />}></Route>
+        <Route path="/menu" element={<Menu />}></Route>
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default App
