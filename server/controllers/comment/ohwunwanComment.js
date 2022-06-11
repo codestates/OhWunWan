85e8335868ab549cf5a6af033de27d7a20a99ba2
jiwoonghwang <hyae4924@gmail.com>
2022-06-12 05:11:56 +0900

    [FEAT] Add api about post of ohwunwan_comments

--- server/controllers/comment/ohwunwanComment.js
+++ server/controllers/comment/ohwunwanComment.js
@@ -1,5 +1,21 @@
-module.exports={
-    post :()=>{},//ohwunwan_comment 작성
-    patch :()=>{},//ohwunwan_comment 수정
-    delete :()=>{},//ohwunwan_comment 삭제
+const { Ohwunwan_comment } = require('../../models')
+module.exports = {
+    //ohwunwan_comment 작성
+    post: async (req, res) => {
+        try {
+            if (!(req.body.ohwunwan_id && req.body.user_id && req.body.text_content)) return res.status(400).json({ message: 'Bad Request!' })
+
+            const { ohwunwan_id, user_id, text_content } = req.body
+            await Ohwunwan_comment.create({ ohwunwan_id, user_id, text_content })
+            res.json({ message: 'The comment has been created' })
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({ message: 'Server Error!' })
+        }
+    },
+
+    //ohwunwan_comment 수정
+    patch: () => { },
+    //ohwunwan_comment 삭제
+    delete: () => { },
 }
