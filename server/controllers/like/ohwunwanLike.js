e2fa0b562b01f13d1d204726a479c387bab9a29a
jiwoonghwang <hyae4924@gmail.com>
2022-06-12 06:26:59 +0900

    [FEAT] Add api about post of ohwunwan_like

--- server/controllers/like/ohwunwanLike.js
+++ server/controllers/like/ohwunwanLike.js
@@ -1,5 +1,18 @@
-module.exports={
+const{Ohwunwan_like}=require('../../models')
 
-    post :()=>{},//ohwunwan 게시물 좋아요
+module.exports={
+    post :async (req,res)=>{
+        try{
+            if(!(req.body.ohwunwan_id&&req.body.user_id)) return res.json({ message: 'Bad Request!' })
+            const{ohwunwan_id,user_id}=req.body
+            await Ohwunwan_like.create({ohwunwan_id,user_id})
+            return res.status(201).json( {message: 'The like has been done'})
+        }catch(err){
+            console.log(err);
+            return res.status(500).json( { message: 'Server Error!' })
+        }
+        
+    },//ohwunwan 게시물 좋아요
     delete :()=>{}//ohwunwan 게시물 좋아요 취소
 }
+
