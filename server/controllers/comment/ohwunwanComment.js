a0e9322e367faa6de445765ba8ce767446b79a92
jiwoonghwang <hyae4924@gmail.com>
2022-06-12 05:23:07 +0900

    [FEAT] Add api about patch of ohwunwan_comments

--- server/controllers/comment/ohwunwanComment.js
+++ server/controllers/comment/ohwunwanComment.js
@@ -15,7 +15,25 @@ module.exports = {
     },
 
     //ohwunwan_comment 수정
-    patch: () => { },
+    patch: async (req, res) => {
+        try{
+            if (!(req.body.ohwunwan_comment_id && req.body.text_content)) return res.status(400).json({ message: 'Bad Request!' })
+    
+            const { ohwunwan_comment_id, text_content } = req.body
+            await Ohwunwan_comment.update(
+                {
+                    text_content,
+                },
+                {
+                    where: { id: ohwunwan_comment_id, }
+                },
+            );
+            return res.json({ message: 'The comment has been changed' })
+        }catch(err){
+            console.log(err)
+            return res.status(500).json({ message: 'Server Error!' })
+        }
+    },
     //ohwunwan_comment 삭제
     delete: () => { },
 }
