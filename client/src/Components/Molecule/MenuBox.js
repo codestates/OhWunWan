7a1db902e38ec805c62970326962430632e5a105
hackjoong <nuclearjoong@gmail.com>
2022-06-13 10:12:14 +0900

    [FEAT] Update Molecule

--- client/src/Components/Molecule/MenuBox.js
+++ client/src/Components/Molecule/MenuBox.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
+import { Fragment } from "react";
 import STYLE from "../../config"
 import SubmitButton from "../Atoms/SubmitButton"
 
 const Div = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.3);
+  position: fixed;
+  z-index: 99;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const Div2 = styled.div`
@@ -14,20 +24,21 @@ const Div2 = styled.div`
 `
 
 function MenuBox() {
+
   return(
-    <Div>
-      <Div2>
-        <SubmitButton text='수정' type='black' />
-      </Div2>
-      <Div2>
-        <SubmitButton text='삭제' type='red' />
-      </Div2>
-      <Div2>
-        <SubmitButton text='취소' type='black' onClick={() => {
-          
-        }} />
-      </Div2>
-    </Div>
+    <Fragment>
+      <Div>
+        <Div2>
+          <SubmitButton text='수정' type='black' />
+        </Div2>
+        <Div2>
+          <SubmitButton text='삭제' type='red' />
+        </Div2>
+        <Div2>
+          <SubmitButton text='취소' type='black' onClick={() => {}} />
+        </Div2>
+      </Div>
+    </Fragment>
   )
 }
 
