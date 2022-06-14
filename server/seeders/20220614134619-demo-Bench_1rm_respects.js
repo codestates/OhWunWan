c32e35c87ce65d87e55e649adcd309ddced8714d
jiwoonghwang <hyae4924@gmail.com>
2022-06-14 23:00:59 +0900

    [FEAT] Add seed about bench

---
+++ server/seeders/20220614134619-demo-Bench_1rm_respects.js
@@ -0,0 +1,108 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Bench_1rm_repects', [
+     {
+      user_id: 1,
+      bench_1rm_id:1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 1,
+      bench_1rm_id:2,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 1,
+      bench_1rm_id:3,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 1,
+      bench_1rm_id:4,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 1,
+      bench_1rm_id:5,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+
+
+
+     {
+      user_id: 3,
+      bench_1rm_id:1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 3,
+      bench_1rm_id:2,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 3,
+      bench_1rm_id:3,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 3,
+      bench_1rm_id:4,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 3,
+      bench_1rm_id:5,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+
+
+
+     {
+      user_id: 4,
+      bench_1rm_id:1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 4,
+      bench_1rm_id:2,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 4,
+      bench_1rm_id:3,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 4,
+      bench_1rm_id:4,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+     {
+      user_id: 4,
+      bench_1rm_id:5,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     },
+  ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Bench_1rm_repects', null, {})
+  }
+};
