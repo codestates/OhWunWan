cbf46602ea7e0978c982eb2de3c6971692e9b43b
jiwoonghwang <hyae4924@gmail.com>
2022-06-14 18:59:27 +0900

    [FIX] delete 주석

--- server/controllers/respect/bench1rmRespect.js
+++ server/controllers/respect/bench1rmRespect.js
@@ -17,33 +17,3 @@ module.exports={
 }
 
 
-
-// const { Feedback_like } = require('../../models')
-// module.exports = {
-//     //feedback 게시물 좋아요  
-//     post: async (req, res) => {
-//         try {
-//             console.log(req.body.feedback_id,req.body.user_id)
-//             if (!(req.body.feedback_id && req.body.user_id)) return res.status(400).json({ message: 'Bad Request!' })
-//             const { feedback_id, user_id } = req.body
-//             await Feedback_like.create({ feedback_id, user_id })
-//             return res.status(201).json({ message: 'The like has been done' })
-//         } catch (err) {
-//             console.log(err);
-//             return res.status(500).json({ message: 'Server Error!' })
-//         }
-//     },
-//     //feedback 게시물 좋아요 취소
-//     delete: async (req, res) => {
-//         try {
-//             const { feedback_like_id } = req.params
-//             await Feedback_like.destroy({
-//                 where: { id: feedback_like_id },
-//             });
-//             return res.json({ message: 'The like has been deleted' })
-//         } catch (err) {
-//             console.log(err);
-//             return res.status(500).json({ message: 'Server Error!' })
-//         }
-//     }
-// }
