d53f45e93594472e7b4866db83a41ba8702ad995
jiwoonghwang <hyae4924@gmail.com>
2022-06-16 17:02:33 +0900

    [FEAT] Add api about get bench_1rm of user

--- server/controllers/user/index.js
+++ server/controllers/user/index.js
@@ -6,7 +6,8 @@ const {
     Feedback, Feedback_comment, Feedback_like,
     User
 } = require('../../models');
-
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 
 module.exports = {
     //user가 작성한ohwunwan 게시물 들고오기
@@ -169,7 +170,123 @@ module.exports = {
             return res.status(500).json({ message: 'Server Error!' })
         }
     },
-    bench_1rm: () => { },
+    //유저가 작성한 bench_1rm 게시물 가져오기
+    bench_1rm: async(req,res) => { 
+        try {
+            //조회할 게시물 페이지들
+            const { count,user_id } = req.params
+
+
+            //1.bench__1rm게시물+user정보 쿼리
+            const bench_1rms = await Bench_1rm.findAll({
+                where: { user_id },
+                attributes: ['id', 'video', 'text_content', 'kg', 'createdAt'],//bench_1rm 컬럼들
+                include: [
+                    {
+                        model: User,
+                        attributes: ['nickname', 'profile_picture'],
+                        require: true,
+                    },
+                ],
+                order: [['id', 'DESC']],//정렬 id순으로 꺼꾸로
+                raw: true,//dataValues만 가져오기
+                limit: 5,//몇개불러올껀가
+                offset: count * 5,//어디서부터시작할껀지
+            });
+
+
+
+            //2. 조회한 게시물들의 고유번호값들 맵핑해서 뽑기
+            const bench_1rm_id = bench_1rms.map(item => item.id)
+            console.log('::::::bench_1rm_id:', bench_1rm_id)
+
+
+
+            //3. 조회한 게시물들의 댓글 + 유저정보 쿼리
+            const bench_1rm_comments = await Bench_1rm_comment.findAll({
+                where: { bench_1rm_id },
+                attributes: ['id', 'bench_1rm_id', 'text_content', 'createdAt'],
+                include: [
+                    {
+                        model: User,
+                        attributes: ['id', 'nickname', 'profile_picture'],
+                        require: true,
+                    }
+                ],
+                raw: true,//dataValues만 가져오기
+            })
+
+
+
+
+            //4.조회한 게시물들 리스펙 정보 쿼리
+            const bench_1rm_respects = await Bench_1rm_respect.findAll({
+                where: { bench_1rm_id },
+                attributes: ['id', 'user_id', 'bench_1rm_id', 'createdAt'],
+                raw: true,//dataValues만 가져오기
+            })
+            // console.log(':::::::::::::bench_1rm_respects:', bench_1rm_respects)
+
+
+
+
+            //5.조회환 게시물들의 랭킹 정보 쿼리
+            const ranks = await Bench_1rm_respect.findAll({
+
+                attributes: ['bench_1rm_id', [sequelize.fn('COUNT', 'bench_1rm_id'), 'respect_count']],
+                group: ['bench_1rm_id'],
+                having: {
+                    'respect_count': { [Op.gte]: 5 }
+                },
+                require: true,
+                raw: true,//dataValues만 가져오기
+                include: [
+                    {
+                        model: Bench_1rm,
+                        attributes: ['kg', [sequelize.literal('(RANK() OVER (ORDER BY kg DESC))'), 'ranking']],
+                        require: true,
+                        raw: true,//dataValues만 가져오기
+
+                    },
+                ],
+
+            });
+            console.log(':::::::::::rank', ranks)
+
+
+
+
+            //6.(조회한게시물+유저정보)+(댓글+유저정보)+리스펙정보+랭킹정보
+            for (const bench_1rm of bench_1rms) {
+                bench_1rm.comment = []
+                bench_1rm.respect = []
+                bench_1rm.rank = []
+                for (const comment of bench_1rm_comments) {
+                    if (bench_1rm.id === comment.bench_1rm_id) {
+                        bench_1rm.comment.push(comment)
+                    }
+                }
+                for (const respect of bench_1rm_respects) {
+                    if (bench_1rm.id === respect.bench_1rm_id) {
+                        bench_1rm.respect.push(respect)
+                    }
+                }
+                for (const rank of ranks) {
+                    if (bench_1rm.id === rank.bench_1rm_id) {
+                        bench_1rm.rank.push(rank)
+                        bench_1rm.rank.push(ranks.length)
+                    }
+                }
+            }
+            console.log(':::::::::::::bench_1rms:', bench_1rms)
+
+            //7. 데이터 보내주기
+            return res.json({ message: 'ok', data: bench_1rms })
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({ message: 'Server Error!' })
+        }
+    },
     dead_1rm: () => { },
     squat_1rm: () => { },
     patch: () => { },
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,9 +4,9 @@ const {user_controller} = require('./../controllers');
 
 router.get('/ohwunwan/:user_id/:count', user_controller.ohwunwan);//ohwunwan 게시물 user 정보요청
 router.get('/feedback/:user_id/:count', user_controller.feedback);//feedback게시물 user 정보요청
-router.get('/1rm/:user_id/:count', user_controller.bench_1rm);//user bench_1rm게시물 정보요청
-router.get('/1rm/:user_id/:count', user_controller.dead_1rm);//user dead_1rm게시물 정보요청
-router.get('/1rm/:user_id/:count', user_controller.squat_1rm);//user squat_1rm게시물 정보요청
+router.get('/bench_1rm/:user_id/:count', user_controller.bench_1rm);//user bench_1rm게시물 정보요청
+router.get('/dead_1rm/:user_id/:count', user_controller.dead_1rm);//user dead_1rm게시물 정보요청
+router.get('/squat_1rm/:user_id/:count', user_controller.squat_1rm);//user squat_1rm게시물 정보요청
 router.patch('/', user_controller.patch);//user 정보수정
 router.delete('/:user_id', user_controller.delete);//회원탈퇴
 
